9a0b7e6fa3a57defffc3314860788f6c62d14468
Viviana <vivi.plc95@hotmail.com>
2021-12-13 22:08:21 -0500

    itinerary draft, itinerary type creation

---
+++ FinalExamSystems/Itinerary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalExamSystems
+{
+    public class Itinerary
+    {
+        private DateTime departureDate
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private DateTime returnDate
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private double totalPrice
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private CreditCard paymentMehod
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private ItineraryType type
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ FinalExamSystems/ItineraryType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalExamSystems
+{
+    public class ItineraryType
+    {
+        private string description
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
