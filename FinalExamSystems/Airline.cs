a6e82fd1d34a5a829e17098e0038fd86186c2840
Romeno Wenogk Fernando <drf325@nyu.edu>
2021-12-16 13:51:13 -0500

    Singleton Pattern applied to Airline Class

--- FinalExamSystems/Airline.cs
+++ FinalExamSystems/Airline.cs
@@ -5,8 +5,34 @@ using System.Text;
 
 namespace FinalExamSystems
 {
+    /// <summary>
+    ///  This Airline class applies the Singleton pattern. Because this is an Airline Reservation System, that is meant for a single airline, we decided to apply to singleton pattern to the airline class, meaning there will only be one instance of the Airline class.
+    /// </summary>
     public class Airline
     {
+        /// <summary>
+        ///  This is the private field that will contain the single instance of the airline class
+        /// </summary>
+        static Airline airlineInstance;
+
+        /// <summary>
+        ///  This is the constructor for the Airline class which cannot be instantiated directly as it is protected
+        /// </summary>
+        protected Airline()
+        {
+        }
+
+        /// <summary>
+        ///  This method returns the singleton instance of the Airline class, if it does not exist it instantiates an instance (this happens only once)
+        /// </summary>
+        public static Airline AirlineInstance()
+        {
+            if (airlineInstance == null)
+            {
+                airlineInstance = new Airline();
+            }
+            return airlineInstance;
+        }
         private string name
         {
             get => default;
